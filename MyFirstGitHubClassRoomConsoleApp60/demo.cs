a950084cb37e5f6325a102649b2daa4a3f4ad1dc
btrhazara@gmail.com <btrhazara@gmail.com>
2025-02-06 12:54:42 +0100

    demo commit

---
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class demo
+        
+    { public string Name { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+     
+        public void Start () 
+        {
+            return; 
+
+        }
+    }
+}
